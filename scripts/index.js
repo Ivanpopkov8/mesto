c95af5b6583ffecc595a3f560b40f2f0597a9f30
Ivanpopkov8 <itsplusooo@yandex.ru>
2022-04-14 20:04:11 +0300

    Сдать проект 6я

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 const popupBtn = document.querySelector('.profile-info__button');
 const modalWindow = document.querySelector('.popup');
 const modalCloseBtn = modalWindow.querySelector('.popup__close-icon');
-let formBoxElement = document.querySelector('.popup__form');
-let titleForInput = document.querySelector('.profile-info__title');
-let subTitleForInput = document.querySelector('.profile-info__subtitle');
-let nameInput = document.querySelector('.popup__filed-name');
-let surNameInput = document.querySelector('.popup__filed-surname');
-let elementContent = document.querySelectorAll('.element__content')
-const whiteHеartIcon = document.querySelectorAll('.element__vector');
+const formBoxElement = document.querySelector('.popup__form');
+const titleForInput = document.querySelector('.profile-info__title');
+const subTitleForInput = document.querySelector('.profile-info__subtitle');
+const nameInput = document.querySelector('.popup__filed-name_one_input');
+const surNameInput = document.querySelector('.popup__filed-name_two_input');
+// const whiteHеartIcon = document.querySelectorAll('.element__vector');
 
 function toggleModalWindow() {
 modalWindow.classList.toggle('popup_is-active');
@@ -40,14 +39,14 @@ function onSubmit(evt) {
 }
 formBoxElement.addEventListener('submit', onSubmit);
 
-whiteHеartIcon.forEach(
-	heartIcon =>  {heartIcon.addEventListener('click', function handleClick(){
-		heartIcon.classList.toggle('element__vector-hover');
+// whiteHеartIcon.forEach(
+// 	heartIcon =>  {heartIcon.addEventListener('click', function handleClick(){
+// 		heartIcon.classList.toggle('element__vector_active');
 		
-	})
+// 	})
 
-	}
-)
+// 	}
+// )
 
 
 
