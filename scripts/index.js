73647b1c75f29ae20042a0852694f1c62e64175a
Ivanpopkov8 <itsplusooo@yandex.ru>
2022-06-14 20:13:52 +0300

    Поправить функцию

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
+//Кнопка редактировать профиль
+
+
 const popupBtn = document.querySelector('.profile-info__button');
+
 const modalWindow = document.querySelector('.popup');
+
 const modalCloseBtn = modalWindow.querySelector('.popup__close-icon');
+
 const formBoxElement = document.querySelector('.popup__form');
+
 const titleForInput = document.querySelector('.profile-info__title');
 const subTitleForInput = document.querySelector('.profile-info__subtitle');
+
 const nameInput = document.querySelector('.popup__filed-name_input_name');
+
 const surNameInput = document.querySelector('.popup__filed-name_input_surname');
-// const whiteHеartIcon = document.querySelectorAll('.element__vector');
+
+
+
 
 function toggleModalWindow() {
 modalWindow.classList.toggle('popup_is-active');
@@ -31,6 +42,7 @@ function onOverLayClick(event) {
 }
 modalWindow.addEventListener('click', onOverLayClick);
 
+// Функция добовления введенного контента в инпут, на страницу
 function onSubmit(evt) {
 	evt.preventDefault();
 	titleForInput.textContent = nameInput.value;
@@ -39,15 +51,111 @@ function onSubmit(evt) {
 }
 formBoxElement.addEventListener('submit', onSubmit);
 
-// whiteHеartIcon.forEach(
-// 	heartIcon =>  {heartIcon.addEventListener('click', function handleClick(){
-// 		heartIcon.classList.toggle('element__vector_active');
+// Работа 6го спринта
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+//Получаю ul
+const elementsContainer = document.querySelector('.elements');
+//Получаю шаблон по id
+const elementsTempl = document.querySelector('#element-template').content;
+//Получаю доступ к элементам шаблона
+// const liContainer = elementsTempl.querySelector('element').cloneNode(true);
+
+// Функция добовления нового массива
+const placeContent = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+
+
+// Метод обхода всех карточек
+function copyCard() {
+  placeContent.forEach(addCard);
+}
+
+// Функция клонирования
+function addCard(item) {
+	console.log(item);
+	// console.log(link);
+	const liContainer = elementsTempl.querySelector('.element').cloneNode(true);
+	liContainer.querySelector(".element__title").textContent = item.name;
+	liContainer.querySelector(".element__image").src = item.link;
+
+  elementsContainer.prepend(liContainer);
+}
+
+
+copyCard();
+
+// Функция клика по like
+const whiteHеartIcon = document.querySelectorAll('.element__vector');
+
+whiteHеartIcon.forEach(
+	heartIcon =>  {heartIcon.addEventListener('click', function handleClick(){
+		heartIcon.classList.toggle('element__vector_active');
 		
-// 	})
+	})
 
-// 	}
-// )
+	}
+)
+
+// Работа над добовление 2го попапа
+
+const formBox = document.querySelector('.form__popup');
+
+const placeInput = document.querySelector('.form__filed-name_input_place');
+
+const placeLimkInput = document.querySelector('.form__filed-name_input_link');
 
 
 
+// Функция открытия попап
+const modalWindowPlace = document.querySelector('.form');
 
+const addButtonPlace = document.querySelector(".add-button");
+
+function toggleModalWindowPlace() {
+	modalWindowPlace.classList.toggle('form_is-active');
+	}
+	addButtonPlace.addEventListener('click', toggleModalWindowPlace);
+
+// Функция закрытия попап крестик
+	const formCloseBtn = modalWindowPlace.querySelector('.form__close-icon');
+	formCloseBtn.addEventListener('click', toggleModalWindowPlace);
+
+	// Функция закрытия попап на любое место экрана
+
+	function onOverClick(event) {
+		if (event.target === event.currentTarget){
+			toggleModalWindowPlace();
+		}
+	}
+	modalWindowPlace.addEventListener('click', onOverClick);
